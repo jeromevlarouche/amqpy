84217744e500b2bf596924dae814f9c093915035
Marc-André Dufresne <marc-andre.dufresne@ormuco.com>
2017-02-07 17:41:50 -0500

    Fixed urllib compatibility with python 3

--- amqpy/connection.py
+++ amqpy/connection.py
@@ -2,7 +2,7 @@
 """
 from __future__ import absolute_import, division, print_function
 
-from urlparse import urlparse, unquote
+from future.moves.urllib.parse import urlparse, urlencode
 
 __metaclass__ = type
 import logging
